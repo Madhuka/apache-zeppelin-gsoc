20b895e874196ae1e5a777682f2fc36b77cb9ec7
madhuka <madhukaudantha@gmail.com>
2015-06-13 17:28:27 +0530

    Using grunt with angular-generator - Code Transition

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,32 +1,30 @@
-var zeppelinGsocApp = angular.module('zeppelinGsocApp', [
-  'ngRoute',
-  'chartControllers',
-  'navControllers'
-]);
-zeppelinGsocApp.factory('Datax', function () {
-    return { dataSet: 'car',
-    getDataSet: function() {
-      return dataSet;
-    },
-    setDataSet: function(name) {
-      dataSet = name;
-    }
-     };
-});
-zeppelinGsocApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/contact', {
-        templateUrl: 'view/contact.html'
-      }).
-      when('/milestone01', {
-        templateUrl: 'view/milestone01.html',
-        controller: 'chartCtrl'
-      }).
-      when('/home', {
-        templateUrl: 'view/about.html'
-      }).
-      otherwise({
-        redirectTo: '/home'
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name apacheZeppelinGsocApp
+ * @description
+ * # apacheZeppelinGsocApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('apacheZeppelinGsocApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
       });
-  }]);
+  });
